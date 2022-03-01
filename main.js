38b80674b745d46727e080ffcd6a5cf7f6850272
ptomao <peter.tomao85@journalism.cuny.edu>
2022-02-28 19:41:46 -0500

    commit changes

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 /**
  * SILENCE IS GOLDEN
  */
+alert("hello, world");
+
+const body = document.querySelector ("body");
+const lightswitches = document.querySelectorAll (".switch");
+
+lightswitches.forEach(function(lightswitch) {
+
+lightswitch.addEventListener("click", function (e) {
+    e.preventDefault(); // prevent the default behavior on the click
+    body.classList.toggle("light-off");
+
+})
+
+}); 
